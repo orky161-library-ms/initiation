332b7ccad3f007dda14098065ee62123755f7bff
orky161 <orky161@gmail.com>
2020-12-04 18:41:35 +0200

    first commit

---
+++ src/execution.py
@@ -0,0 +1,29 @@
+import sys
+
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
+host = os.environ.get("DB_HOST")
+port = int(os.environ.get("DB_PORT"))
+user = os.environ.get("DB_USER")
+passwd = os.environ.get("DB_PASSWORD")
+dbName = os.environ.get("DB_NAME")
+
+mydb = mysql.connector.connect(host=host, port=port, user=user, passwd=passwd, db=dbName)
+
+with open(os.path.join(sys.path[0], "full-script"), "r") as f:
+    query = f.read()
+
+cursor = mydb.cursor(buffered=True)
+results = cursor.execute(query, multi=True)
+
+for cur in results:
+    print('cursor:', cur)
+    if cur.with_rows:
+        print('result:', cur.fetchall())
+
+mydb.commit()
+mydb.close()
